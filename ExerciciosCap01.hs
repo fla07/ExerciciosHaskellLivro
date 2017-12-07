1ce50eff7dddfea5ad83e564cfb7a72f7f2850ff
Flavia Nunes Pino <flavitta007@gmail.com>
2017-12-07 01:26:20 +0000

    Capitulo 01

---
+++ ExerciciosCap01.hs
@@ -0,0 +1,25 @@
+module ExerciciosCap01 where
+
+-- Capitulo 01 -> Programação Funcional
+-- Aqui vou colocar alguns exemplos simples sobre o que é o Haskell,
+e como usar o GHCI. O livro em si não possui exercicios no capítulo 1.
+
+-- Tudo o que vem a seguir pode ser testado no GHCI
+
+-- Exemplo do conceito de lazyness do livro:
+-- A linguagem só calcula algo quando for extremamente necessário
+-- Isso poupa tempo e processamento
+let a = [3, 6 , 7, 3*10^89,0] ++ [-1,9]
+
+-- O terminal pode ser usado para realizar operações matemáticas de modo 
+-- convencional
+let b = 3
+let c = 4
+let d = b + c
+let e = c - b
+let f = b*c
+let g = d / b
+let h = b^2
+
+-- Note que não existem variáveis, o que temos são funções que uma vez que
+-- adquirem um valor, não mudam.
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Exercíos extraídos do livro# ExerciciosHaskellLivro
+
+Aqui serão usados exemplos do Livro do Prof Alexandre Garcia e alguns outros
+dados em aula.
+O objetivo é aprender de uma vez e passar na prova.
+
+para ver os exemplos rodando via terminal:
+ghci
+carregando o arquivo:
+:l Nome_Arquivo.hs
+ou recarregando:
+:r 
